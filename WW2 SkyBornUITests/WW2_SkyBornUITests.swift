0e7ae4c501b31b4870580f324ab2f38d5a0338be
Michael Timpson <Michael@michaels-mbp.home>
2016-05-05 17:13:47 -0400

    Initial Commit

---
+++ WW2 SkyBornUITests/WW2_SkyBornUITests.swift
@@ -0,0 +1,36 @@
+//
+//  WW2_SkyBornUITests.swift
+//  WW2 SkyBornUITests
+//
+//  Created by Michael Timpson on 5/5/16.
+//  Copyright © 2016 newBorn Software Development Company. All rights reserved.
+//
+
+import XCTest
+
+class WW2_SkyBornUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
