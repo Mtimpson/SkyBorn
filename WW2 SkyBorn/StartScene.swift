53287cbca501651ebbd6c1d7af1abfb4e7fbd3f0
Michael Timpson <Michael@michaels-mbp.home>
2016-05-09 22:12:04 -0400

    Tried adding 2 new buttons for a start screen, made a start screen class.

---
+++ WW2 SkyBorn/StartScene.swift
@@ -0,0 +1,18 @@
+//
+//  StartScene.swift
+//  WW2 SkyBorn
+//
+//  Created by Michael Timpson on 5/9/16.
+//  Copyright © 2016 newBorn Software Development Company. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class StartScene: UIViewController  {
+    
+    //Class to start the game!
+    
+    
+}
